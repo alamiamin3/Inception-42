5075cdc41b0dbfe206308a89b2cb15dfaaca0489
Ahmed Amine Alami <aalami@e2r7p13.1337.ma>
2024-06-09 22:21:07 +0100

    maria-db fixed

--- wordpress/tools/script.sh
+++ wordpress/tools/script.sh
@@ -7,8 +7,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 chmod 777 .
-tail -f
-# wp core download --allow-root
-# wp config create --dbname=db --dbuser=root --dbpass=example --dbhost=maria-db --allow-root
-# wp core install --url=https://localhost:443 --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org --allow-root
-# php-fpm7.4 -F
+wp core download --allow-root
+wp config create --dbname=db --dbuser=root --dbpass=example --dbhost=maria-db --allow-root
+wp core install --url=https://127.0.0.1 --title="WP-CLI" --admin_user=wpcli --admin_password=wpcli --admin_email=info@wp-cli.org --allow-root
+php-fpm7.4 -F
